278279ae1fda192de4dfd813263e8151be0476bd
Paweł Kocan <pkocan@onet.pl>
2022-03-13 22:38:32 +0100

    Template method pattern homework implementation

---
+++ src/main/java/pl/kocan/template/method/pattern/homework/Main.java
@@ -0,0 +1,11 @@
+package pl.kocan.template.method.pattern.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WeekDay day = new MyDay();
+        day.everyDayIsExaclyTheSame(TypeOfTransport.BIKE);
+
+    }
+}
---
+++ src/main/java/pl/kocan/template/method/pattern/homework/TypeOfTransport.java
@@ -0,0 +1,8 @@
+package pl.kocan.template.method.pattern.homework;
+
+public enum TypeOfTransport {
+
+    CAR,
+    TRAM,
+    BIKE
+}
